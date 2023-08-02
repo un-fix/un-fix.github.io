08ba4127f6f58b4f7b41266ec0941f0c43356976
changweihua <changweihua@outlook.com>
2023-08-02 16:54:02 +0800

    chore: 增加CI

--- .dumirc.ts
+++ .dumirc.ts
@@ -1,7 +1,16 @@
 import { defineConfig } from 'dumi';
 
+const repo = 'un-fix.github.io';
+
 export default defineConfig({
   themeConfig: {
     name: 'un-fix.github.io',
   },
+  title: 'un-fix.github.io',
+  mode: 'site',
+  // devServer: {
+  //   port: 1998, // 自定义端口号
+  // },
+  base: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
+  publicPath: process.env.NODE_ENV === 'production' ? `/${repo}/` : '/',
 });
