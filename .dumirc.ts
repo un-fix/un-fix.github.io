d62e374673ccd0b7bfd70081656532aefee6f737
un-fix <141185749+un-fix@users.noreply.github.com>
2023-08-02 14:03:00 +0000

    Deploying to build from @ un-fix/un-fix.github.io@5687dc84c5518b1770324feb5ac362e9ce797c48 🚀

--- .dumirc.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from 'dumi';
-
-const repo = 'un-fix.github.io';
-
-export default defineConfig({
-  themeConfig: {
-    name: 'un-fix.github.io',
-  }
-});
---
+++ dumi__pages__Demo.c2da8793.async.js
@@ -0,0 +1,1 @@
+"use strict";(self.webpackChunkun_fix_github_io=self.webpackChunkun_fix_github_io||[]).push([[448],{65149:function(c,n,e){e.r(n),e.d(n,{default:function(){return r}});var t=e(14570),a=e(67294),o=function(){var d=(0,t.UO)(),i=d.id,m=(0,t.WF)(),s=m.demos,f=s[i]||{},u=f.component;return u&&(0,a.createElement)(u)},r=o}}]);
